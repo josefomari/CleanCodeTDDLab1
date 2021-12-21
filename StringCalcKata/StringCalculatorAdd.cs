049d74025cb473b4a58cf03fe05901abe52b772c
josefomari <josef401@hotmail.com>
2021-12-21 14:21:16 +0100

    Add project files.

---
+++ StringCalcKata/StringCalculatorAdd.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace StringCalcKata
+{
+    public class StringCalculatorAdd
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
